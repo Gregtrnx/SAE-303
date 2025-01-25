c38ee6e141ad4cf5b7d93932ec7d29578d57600f
Gregtrnx <146743037+Gregtrnx@users.noreply.github.com>
2025-01-25 14:17:40 +0100

    insertion section graphique

---
+++ scripts/index-script.js
@@ -0,0 +1,19 @@
+
+const backToTopButton = document.getElementById("backToTop");
+
+// Affiche le bouton lorsque l'utilisateur descend
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) { // Seuil d'affichage
+        backToTopButton.style.display = "flex";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+});
+
+// Action de remontée en haut
+backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
